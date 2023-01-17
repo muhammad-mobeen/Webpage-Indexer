79248ee19a9751491d417938605aab88b22ac148
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2023-01-18 03:15:52 +0500

    Baser

---
+++ OAuth2Authenticator.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Newtonsoft.Json.Linq;
+
+namespace Search_Console_API
+{
+    public static class OAuth2Authenticator
+    {
+        public static UserCredential Authenticate(string oauthClient, string oauthSecret, string[] scopes, bool saveState = true)
+        {
+            if (oauthClient == null || oauthSecret == null || scopes == null)
+                throw new ArgumentNullException();
+            try
+            {
+                UserCredential credentials = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    new ClientSecrets
+                    {
+                        ClientId = oauthClient,
+                        ClientSecret = oauthSecret,
+                    },
+                    scopes, "user", CancellationToken.None).Result;
+
+                if (saveState) SaveState(credentials);
+                return credentials;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public static UserCredential Authenticate(string jsonPath, string[] scopes, bool saveState = true)
+        {
+            JObject jsonFile = JObject.Parse(File.ReadAllText(jsonPath));
+            string clientId = jsonFile["installed"]?["client_id"]?.ToString();
+            string clientSecret = jsonFile["installed"]?["client_secret"]?.ToString();
+            return Authenticate(clientId, clientSecret, scopes);
+        }
+
+        private static UserCredential LoadExistingGoogleToken(string oauthClient, string oauthSecret)
+        {
+            try
+            {
+                GoogleAuthorizationCodeFlow flow = CreateAuthenticationFlow(oauthClient, oauthSecret);
+                JObject token = JObject.Parse(File.ReadAllText("googleToken.json"));
+                TokenResponse tokenResponse = new TokenResponse()
+                {
+                    AccessToken = token["accessToken"]?.ToString() ?? throw new ArgumentNullException(),
+                    RefreshToken = token["refreshToken"]?.ToString() ?? throw new ArgumentNullException(),
+                    ExpiresInSeconds = long.Parse(token["expiresInSeconds"]?.ToString() ?? throw new ArgumentNullException()),
+                    IdToken = token["idToken"]?.ToString() ?? throw new ArgumentNullException(),
+                    IssuedUtc = DateTime.Parse(token["issuedUtc"]?.ToString()),
+                    Scope = token["scope"]?.ToString() ?? throw new ArgumentNullException(),
+                    TokenType = token["tokenType"]?.ToString() ?? throw new ArgumentNullException()
+                };
+                UserCredential credentials = new UserCredential(flow, "user", tokenResponse);
+                return credentials;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        private static GoogleAuthorizationCodeFlow CreateAuthenticationFlow(string oauthClient, string oauthSecret)
+        {
+            var authorization = new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = oauthClient,
+                    ClientSecret = oauthSecret
+                }
+            };
+            var flow = new GoogleAuthorizationCodeFlow(authorization);
+            return flow;
+        }
+
+        public static UserCredential ReAuthenticate(UserCredential token, bool saveState = true)
+        {
+            if (token == null)
+                throw new ArgumentNullException(nameof(token));
+            try
+            {
+                GoogleWebAuthorizationBroker.ReauthorizeAsync(token, CancellationToken.None).Wait();
+                if (saveState) SaveState(token);
+                return token;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        private static void SaveState(UserCredential credential)
+        {
+            JObject googleToken = new JObject()
+            {
+                new JProperty("accessToken", credential.Token.AccessToken),
+                new JProperty("refreshToken", credential.Token.RefreshToken),
+                new JProperty("expiresInSeconds", credential.Token.ExpiresInSeconds),
+                new JProperty("idToken", credential.Token.IdToken),
+                new JProperty("issuedUtc", credential.Token.IssuedUtc.ToString("yyyy-MM-dd hh:mm:ss.fff")),
+                new JProperty("tokenType", credential.Token.TokenType),
+                new JProperty("scope", credential.Token.Scope)
+            };
+
+            File.WriteAllText("googleToken.json", googleToken.ToString());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,63 +1,42 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
-using Google.Apis.Webmasters.v3;
-using Google.Apis.Webmasters.v3.Data;
-
+﻿using System;
 using System.Collections.Generic;
-var credentialsPath = "./service-key-filename.json";
-var searchConsoleUrl = "sc-domain:yourdomain.com";
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.SearchConsole.v1;
+using Google.Apis.SearchConsole.v1.Data;
+using Google.Apis.Services;
 
-var stream = new FileStream(credentialsPath, FileMode.Open);
-
-var credentials = GoogleCredential.FromStream(stream);
-
-if (credentials.IsCreateScopedRequired)
+namespace Search_Console_API
 {
-    credentials = credentials.CreateScoped(new string[] { WebmastersService.Scope.Webmasters });
-}
-
-var service = new WebmastersService(new BaseClientService.Initializer()
-{
-    HttpClientInitializer = credentials,
-    ApplicationName = "Console App"
-});
-
-DateTime startDate = DateTime.Now.AddDays(-501);
-DateTime endDate = DateTime.Now;
-
-List dimensionList = new List();
-dimensionList.Add("query");
-            
-var request = new SearchAnalyticsQueryRequest();
-
-request.StartDate = startDate.ToString("yyyy-MM-dd");
-request.EndDate = endDate.ToString("yyyy-MM-dd");
-request.Dimensions = dimensionList;
-
-var response = service.Searchanalytics.Query(request, searchConsoleUrl).Execute();
-
-StringBuilder sb = new StringBuilder();
-
-sb.AppendLine("Query,Clicks,Impressions,CTR,AvgPosition");
-
-if(response.Rows != null)
-{
-    foreach(var row in response.Rows)
+    internal class Program
     {
-        sb.Append(row.Keys[0]);
-        sb.Append(",");
-        sb.Append(row.Clicks);
-        sb.Append(",");
-        sb.Append(row.Impressions);
-        sb.Append(",");
-        sb.Append(row.Ctr);
-        sb.Append(",");
-        sb.Append(row.Position);
-        sb.AppendLine();
+        static void Main(string[] args)
+        {
+            var credentials = OAuth2Authenticator.Authenticate("google_oauth_credentials.json", new [] {SearchConsoleService.Scope.Webmasters});
+            
+            using (var service = new SearchConsoleService(new BaseClientService.Initializer() { HttpClientInitializer = credentials }))
+            {
+                var websiteList = service.Sites.List().Execute();
+                foreach (var site in websiteList.SiteEntry)
+                {
+                    Console.WriteLine(site.SiteUrl + " | Fetching keywords:");
+                    var keywords = service.Searchanalytics.Query(new SearchAnalyticsQueryRequest()
+                    {
+                        StartDate = "2021-01-01",
+                        EndDate = "2021-10-01",
+                        Dimensions = new List<string>() {"QUERY"}
+                    }, site.SiteUrl).Execute();
+
+                    if (keywords.Rows != null)
+                        foreach (var keyword in keywords.Rows) 
+                            foreach (string k in keyword.Keys)
+                                Console.WriteLine(k + " | Impressions: " + keyword.Impressions + " | Clicks: " + keyword.Clicks + " | Ctr: " + keyword.Ctr + " | Position: " + keyword.Position);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadLine();
+        }
     }
 }
-
-File.WriteAllText("queries.csv", sb.ToString());
